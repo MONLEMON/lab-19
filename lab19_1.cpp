4ba7e53c94d17ec521b814926e05702312f2e444
Surabordin <bluelimec@gmail.com>
2022-01-25 23:23:14 +0700

    lab19 complete

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,56 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,  vector<string> &names, vector<int> &scores,vector<char> &grades){
+    ifstream fin(filename);
+    string textline;
+    while(getline(fin,textline)){
+    char name[100];
+    int a,b,c;
+    char z[] = "%[^:]: %d %d %d";
+    sscanf(textline.c_str(),z,name,&a,&b,&c);
+    names.push_back(name);
+    scores.push_back(a+b+c);
+    grades.push_back(score2grade(a+b+c));
+    }
 
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+string line;
+char commander[100];
+char keyer[100];
+char z[] = "%s %[^\n]";
+cout << "Please input your command: ";
+getline(cin , line);
+sscanf(line.c_str() , z, commander , keyer);
+command = commander;
+key = keyer;
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores,vector<char> grades , string key){
+cout << "---------------------------------\n";
+int furit = -1 ;
+for(unsigned int i=0 ; i<names.size() ; i++){
+    if (toUpperStr( names.at(i)) == key ) furit =i ;
+}
+if(furit != -1){
+    cout << names.at(furit) << "'s score = "<< scores.at(furit) << "\n";
+    cout << names.at(furit) << "'s grade = "<< grades.at(furit) << "\n";
+}else{
+    cout << "Cannot found.\n";
+}
+cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names, vector<int> scores,vector<char> grades , string key){
+cout << "---------------------------------\n";
+for (unsigned int i=0 ; i<grades.size() ; i++){
+    if(grades.at(i) == *key.c_str()){
+        cout << names.at(i) << " (" << scores.at(i) << ")\n";
+    }
+}
+cout << "---------------------------------\n";
 }
 
 
